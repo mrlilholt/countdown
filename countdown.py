85da0f3e59d8b2b8ba94bf96817195eaa21ae2c6
mrlilholt <63080594+mrlilholt@users.noreply.github.com>
2023-11-10 07:46:07 -0500

    Add files via upload
    python upload (600 seconds)

---
+++ countdown.py
@@ -0,0 +1,42 @@
+import time
+
+# Set the countdown duration in seconds (10 minutes)
+countdown_duration = 10 * 60
+
+while countdown_duration > 0:
+    print(f"Countdown: {countdown_duration} seconds")
+    time.sleep(1)  # Wait for 1 second
+    countdown_duration -= 1
+
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
+print("Countdown complete!")
+print("Time To TEST!")
